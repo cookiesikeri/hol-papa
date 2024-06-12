fcfbdd50ae8c93e54b425076ccbe101cfa89c0fe
cookiesikeri <ebenezer.ikeri@bankly.ng>
2024-06-12 13:59:42 +0100

    jhg

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,7 +3,7 @@
       <div class="row">
         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
           <div class="logo">
-            <a href="{{url('/')}}"><img src="../../images/Yinka_Yusuf_Logo_Design_One__1_-removebg-preview.png" alt="logo" height="85" width="90"></a>
+            <a href="{{url('/')}}"><img src="../../images/Yinka_Yusuf_Logo_Design_One__1_-removebg-preview.png" alt="logo" height="85" width="150"></a>
           </div>
         </div>
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
